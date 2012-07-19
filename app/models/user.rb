5191f142160c8dfb55b8afe950105398045c38bb
VRWebworx <vrwebworx@gmail.com>
2012-07-18 22:02:16 -0600

    Yea I have it working up to last Friday (7/13/2012 - class 15)

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+  attr_accessible(:playerid, :first_name, :last_name, :email)
+
+  validates(:playerid, :presence => true)
+  validates(:first_name, :presence => true)
+  validates(:last_name, :presence => true)
+  validates(:email, :presence => true)
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiscTracker::Application.config.secret_token = 'ee15eb54ca4630c7e90061725a7c8f40534e081b5ab34c9321a5a91a1d198e66437c9b6ec09fb9e564c1351dc093cbf7d49577a92b25b4939cb940eea2c25871'
